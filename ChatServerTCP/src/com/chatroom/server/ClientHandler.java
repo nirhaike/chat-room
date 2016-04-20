1f105a5cc78d6da5c3f7bffc0882f4a7d6da941e
Eran <eran.hermosh@gmail.com>
2016-04-20 22:15:35 +0300

    send function

--- ChatServerTCP/src/com/chatroom/server/ClientHandler.java
+++ ChatServerTCP/src/com/chatroom/server/ClientHandler.java
@@ -1,5 +1,7 @@
 package com.chatroom.server;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
@@ -8,33 +10,41 @@ public class ClientHandler implements Runnable {
 	public static final String CLIENT_RES = "servergood";
 	public static final String CLIENT_ACK = "clientcheck";
 	public static final String SERVER_RES = "clientgood";
-	
+
 	private Socket socket;
 	private boolean connected;
-	
+
 	public ClientHandler(Socket s) {
 		this.socket = s;
 		connected = true;
 	}
-// send, receive, close, check	
+
+	// send, receive, close, check
 	public void run() {
 		while (connected) {
 			// TODO ...
 		}
 	}
-	
+
 	public void close() throws IOException {
-		
+		this.socket.close();
 	}
-	
+
 	/**
 	 * @pre data != null
 	 * @post $ret: whether the message was sent
 	 */
 	public boolean send(String data) {
-		return false;
-	}
-	
-	
-
+		
+		try{
+		out = new PrintWriter(socket.getOutputStream(), true);
+		out.println(data);
+		out.flush();
+		}
+		catch (IOException e) {
+			return false;
+		}
+		return true;
+		
+		}
 }
