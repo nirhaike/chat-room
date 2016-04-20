6b6731fc188db635341d5ead86692766d2b172f2
Eran <eran.hermosh@gmail.com>
2016-04-20 22:06:53 +0300

    try

--- ChatServerTCP/src/com/chatroom/server/ClientHandler.java
+++ ChatServerTCP/src/com/chatroom/server/ClientHandler.java
@@ -6,7 +6,7 @@ public class ClientHandler implements Runnable {
 
 	public static final String SERVER_ACK = "servercheck";
 	public static final String CLIENT_RES = "servergood";
-	
+	// try;
 	public static final String CLIENT_ACK = "clientcheck";
 	public static final String SERVER_RES = "clientgood";
 	
