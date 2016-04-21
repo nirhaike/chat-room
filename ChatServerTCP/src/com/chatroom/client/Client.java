057eee040ccd83e3f54a3a61a4bb711b4464c533
nirhaike <niruser12@gmail.com>
2016-04-21 23:32:39 +0300

    Updated client

--- ChatServerTCP/src/com/chatroom/client/Client.java
+++ ChatServerTCP/src/com/chatroom/client/Client.java
@@ -6,42 +6,98 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.chatroom.Utils;
 
 public class Client implements Runnable {
 	
+	public static final String CONNECTION_CLOSED = "#CLOSED_CONNETION";
+	
+	public static final String SERVER_ACK = "servercheck";
+	public static final String CLIENT_RES = "servergood";
+	
+	public static final String CLIENT_ACK = "clientcheck";
+	public static final String SERVER_RES = "clientgood";
+	
+	
 	private Socket s;
 	private PrintWriter writer;
 	private BufferedReader reader;
 	
+	// arraylist of last acknowledge messages 
+	private ArrayList<String> ack;
+	
+	private boolean active;
+	
 	public Client(String addr, int port) throws UnknownHostException, IOException {
 		s = new Socket(addr, port);
 		writer = new PrintWriter(s.getOutputStream());
-		reader = new BufferedReader(new InputStreamReader(s.getInputStream())); 
+		reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		active = false;
 	}
+	
+	public void send(String message) {
+		writer.write(message);
+		writer.flush();
+	}
+	
+	public String recv() {
+		String msg;
+		while (true) {
+			try {
+				msg = reader.readLine();
+			} catch (IOException e) {
+				System.out.println("Lost connection with the remote server");
+				return CONNECTION_CLOSED;
+			}
+			if (isAcknowledgeResponse(msg)) {
+				ack.add(msg);
+			} else if (isAcknowledgeRequest(msg)) {
+				// respond to the acknowledge
+				
+			} else {
+				return msg;
+			}
+		}
+	}
+	
+	/**
+	 * @param str a message from the server
+	 * @return whether this message is an acknowledge request response
+	 */
+	public boolean isAcknowledgeResponse(String str) {
+		return str.equals(SERVER_RES);
+	}
+	
+	/**
+	 * @param str a message from the server
+	 * @return whether this message is an acknowledge request request
+	 */
+	public boolean isAcknowledgeRequest(String str) {
+		return str.equals(SERVER_ACK);
+	}
+
 	
 	public void run() {
 		// handle the handshake
 		String handshake = "";
-		try {
-			handshake = reader.readLine();
-		} catch (IOException e) {
-			System.out.println("Couldn't connect to the server...");
-		}
+		handshake = recv();
 		// if the handshake is not correct
 		if (!handshake.equals(Utils.getDate())) {
 			close();
 			return;
 		}
 		// send the handshake
-		writer.write(Utils.changeDateHandShake(handshake));
+		send(Utils.changeDateHandShake(handshake));
+		// the socket is now active
+		active = true;
 		// init the scanner
 		Scanner sc = new Scanner(System.in);
 		String msg = sc.nextLine();
 		while (!msg.equals("-q")) {
-			writer.write(msg);
+			send(msg);
 			// get the next message from the client
 			msg = sc.nextLine();
 		}
@@ -64,6 +120,11 @@ public class Client implements Runnable {
 		} catch (IOException e) {
 			System.err.println("Error occured while closing the client socket.");
 		}
+		active = false;
+	}
+	
+	public boolean isActive() {
+		return active;
 	}
 	
 }
--- ChatServerTCP/src/com/chatroom/client/Main.java
+++ ChatServerTCP/src/com/chatroom/client/Main.java
@@ -1,9 +1,36 @@
 package com.chatroom.client;
 
-public class Main {
+import java.io.IOException;
 
+public class Main {
+	
 	public static void main(String[] args) {
-		
+		// parse the arguments
+		String ip = args[0];
+		int port = Integer.parseInt(args[1]);
+		// initialize the client handler
+		Client c = null;
+		try {
+			c = new Client(ip, port);
+		} catch (IOException e) {
+			System.err.println("Couldn't set up the client");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		// start the client handler
+		Thread t = new Thread(c);
+		t.start();
+		// send the acknowledges
+		while (c.isActive()) {
+			// sleep 5 seconds
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			// send acknowledge
+			c.send(Client.CLIENT_ACK);
+		}
 	}
 	
 }
