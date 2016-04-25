a9ad968171fdcb9775d1d5b02e4c9b5e7f89e176
nirhaike <niruser12@gmail.com>
2016-04-25 18:42:49 +0300

    completed handshake format

--- ChatServerTCP/src/com/chatroom/Utils.java
+++ ChatServerTCP/src/com/chatroom/Utils.java
@@ -1,5 +1,11 @@
 package com.chatroom;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class Utils {
 
@@ -9,18 +15,31 @@ public class Utils {
 	 * h = hour
 	 * @return the handShake
 	 */
-	public static String changeDateHandShake(String d){
-		// TODO implement that
-		return "";
-		
+	public static String changeDateHandShake(String str){
+		DateFormat format = new SimpleDateFormat("yyyy/MM/dd/HH");
+		Date d;
+		try {
+			d = format.parse(str);
+		} catch (ParseException e) {
+			return "---";
+		}
+		Date hourBef = new Date(d.getTime()-1000*60*60);
+		return format.format(hourBef);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getDate());
+		System.out.println(changeDateHandShake(getDate()));
 	}
 	
 	/**
 	 * @return the current date with the format "yyyy/mm/dd/hh"
 	 */
 	public static String getDate() {
-		// TODO implement that
-		return "";
+		Calendar cal = Calendar.getInstance();
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd/HH");
+		Date d = cal.getTime();
+		return df.format(d);
 	}
 
 }
